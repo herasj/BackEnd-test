7d028362c2d9511920cc48e8a2a67642dbbaafa0
herasj <juanp231100@gmail.com>
2020-03-21 22:21:05 -0500

    Express and MongoDB connection

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const authRouter = require('./routes/authorization');
-const usersRouter = require('./routes/users');
-const messageRouter = require('./reoutes/messages')
-
-const app = express();
-
+const app = require('./config/express');
+const db = require('./config/mongo');
+require('dotenv');
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Define routes
-app.use('/users', usersRouter);
-app.use('/authorization', authRouter);
-app.use('/messages', messageRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+db.then(
+    () => {
+      console.log('Connected to MongoDB')
+      express.listen(process.env.API_PORT, () => console.log(`Running API on port ${process.env.API_PORT}`))
+    },
+    error => console.warn(error)
+  )
+  .catch(err => console.log(err));
+  
 
 module.exports = app;
---
+++ config/express.js
@@ -0,0 +1,45 @@
+const app = express();
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+//Basic express middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+
+//Routes
+const authRouter = require('../routes/authorization');
+const usersRouter = require('../routes/users');
+const messageRouter = require('../routes/messages');
+
+//Define routes
+app.use('/users', usersRouter);
+app.use('/authorization', authRouter);
+app.use('/messages', messageRouter);
+
+
+module.exports = app;
